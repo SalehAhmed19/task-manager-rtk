448aaa6f6d2ac08fd8494923048f0890077e52f6
Saleh Ahmed Mahin <saleh.ahmed.mahin@gmail.com>
2025-07-01 11:37:47 +0600

    set-modal

--- src/components/tasks/MyTasks.jsx
+++ src/components/tasks/MyTasks.jsx
@@ -1,27 +1,27 @@
 import {
   CheckIcon,
   DocumentMagnifyingGlassIcon,
-} from '@heroicons/react/24/outline';
+} from "@heroicons/react/24/outline";
 
 const MyTasks = () => {
   const item = {
     id: 1,
-    status: 'pending',
-    title: 'Remove Button',
+    status: "pending",
+    title: "Remove Button",
     description:
-      'We need a remove button in our task card. Meke the button red and use Heroicon for tashbin icon.',
-    date: '2023-08-28',
-    assignedTo: 'Mir Hussain',
-    priority: 'high',
+      "We need a remove button in our task card. Meke the button red and use Heroicon for tashbin icon.",
+    date: "2023-08-28",
+    assignedTo: "Mir Hussain",
+    priority: "high",
   };
 
   return (
     <div>
-      <h1 className="text-xl my-3">My Tasks</h1>
+      <h1 className="my-3 text-xl">My Tasks</h1>
       <div className=" h-[750px] overflow-auto space-y-3">
         <div
           key={item.id}
-          className="bg-secondary/10 rounded-md p-3 flex justify-between"
+          className="flex justify-between p-3 rounded-md bg-secondary/10"
         >
           <h1>{item.title}</h1>
           <div className="flex gap-3">
